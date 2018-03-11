7b0703bcb27495386fdbdc331f1c058178703dcb
johnson <poiuytrengo@qq.com>
2018-03-11 11:15:15 +0800

    分类管理--添加生活服务分类

--- application/admin/controller/Category.php
+++ application/admin/controller/Category.php
@@ -1,9 +1,14 @@
 <?php
 namespace app\admin\controller;
 use think\Controller;
+use think\Model;
 
 class Category extends Base
 {
+    private  $objmodel;
+    public function initialize(){
+        $this->objmodel = model('Category');
+    }
     public function index()
     {
         return $this->fetch();
@@ -17,10 +22,18 @@ class Category extends Base
     {
         $data = input('post.');
 //        dump($data);die();
+        //数据校验
         $validate = validate('Category');
         if(!$validate->check($data)){
             $this->error($validate->getError());
         }
+        if($this->objmodel->addClassification($data)){
+            $this->success('生活服务类添加成功');
+        }else{
+
+            $this->error('生活服务类添加失败');
+        }
+        return;
     }
 
 }
---
+++ application/admin/model/Category.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Johnson
+ * Date: 2018/3/9
+ * Time: 9:30
+ */
+
+namespace app\admin\model;
+use think\Model;
+
+class Category extends Model
+{
+    /**
+     *分类管理--添加生活服务分类
+     */
+    public function addClassification($data){
+        if(empty($data) || !is_array($data)){
+            return false;
+        }
+        if($this->save($data)){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     *获取一级分类
+     */
+    public function getFirstCategory(){
+        $data = [
+            'parent_id' => 0,
+            'status' =>['neq',-1],
+        ];
+        $order = [
+            'id' => 'desc',
+        ];
+        $result = $this->where($data)->order($order)->select();
+        echo $this->getLastSql();
+        return $result;
+    }
+}
